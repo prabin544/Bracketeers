6d6b327d254a4447635ce7156619983a061d735e
Prabin Singh <pravin544@gmail.com>
2020-10-16 20:39:18 -0500

    Added padding css

--- public/populate.js
+++ public/populate.js
@@ -13,19 +13,14 @@ $(document).ready(function () {
         }
         $("#eastBtn").click(function (event) {
             event.preventDefault();
-            console.log(([...$('#dropdown option')].find(select => select.selected).value.replace('_', " ")));
-            var selected = ([...$('#dropdown option')].find(select => select.selected).value.replace('_', " "));
-            // $.ajax({
-            //     url: "http://localhost:8090/addeast",
-            //     type: "POST",
-            //     data: {
-            //         'Name': selected,
-            //     },
-            //     success: function(data){
-            //         console.log(data);
-            //     }
-            // });
-            $.post("/addeast", {'Name': selected})
+            //console.log(($('#dropdown-two option').find(select => select.selected)));
+            console.log($("#dropdown option").filter(":selected"));
+            var list = [];
+            $("#dropdown option").filter(":selected").each(function(east){
+                list.push($(this).val());
+            });
+            console.log(list);
+            $.post("/addeast", {'NameList': list})
                 .then(function(){
                     console.log("Added country");
                 });
@@ -35,29 +30,27 @@ $(document).ready(function () {
         $("#westBtn").click(function (event) {
             // console.log([...$('#dropdown option')].find(select => select.selected).value.replace('_', " "))
             event.preventDefault();
-            console.log(([...$('#dropdown-two option')].find(select => select.selected).value.replace('_', " ")));
-            var selected = ([...$('#dropdown-two option')].find(select => select.selected).value.replace('_', " "));
-            console.log(selected);
-            // $.ajax({
-            //     url: "http://localhost:8090/addwest",
-            //     type: "POST",
-            //     data: {
-            //         'Name': selected,
-            //     },
-            //     success: function(data){
-            //         console.log(data);
-            //     }
-            // });
-            $.post("/addwest", {'Name': selected})
+            //console.log(($('#dropdown-two option').find(select => select.selected)));
+            console.log($("#dropdown-two option").filter(":selected"));
+            var list = [];
+            $("#dropdown-two option").filter(":selected").each(function(west){
+                list.push($(this).val());
+            });
+
+            // var selected = ([...$('#dropdown-two option')].find(select => select.selected).value.replace('_', " "));
+            // var list = $("#westtList").val();
+            console.log(list);
+            
+            $.post("/addwest", {'NameList': list})
                 .then(function(){
                     console.log("Added country");
                 });
         });
         // console.log(results);
         // Putting place holder text in the drop-down menu
-        const placeHolder = '<option value="" selected disabled hidden>Choose Your Country</option>';
-        $('#dropdown select').append(placeHolder);
-        $('#dropdown-two select').append(placeHolder)
+        // const placeHolder = '<option value="" selected disabled hidden>Choose Your Country</option>';
+        // $('#dropdown select').append(placeHolder);
+        // $('#dropdown-two select').append(placeHolder)
         for (var i = 0; i < results.length; i++) {
             $('#dropdown select').append('<option value=' + results[i].replace(' ', '_') + ' name=' + results[i].replace(' ', '_') + '>' + results[i] + '</option>');
             $('#dropdown-two select').append('<option value=' + results[i].replace(' ', '_') + ' name=' + results[i].replace(' ', '_') + '>' + results[i] + '</option>')
